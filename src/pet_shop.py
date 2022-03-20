3d5b3a0be20341b37b4e434cf902c7c1208bb4d7
Speirsy <jpspeirs@icloud.com>
2022-03-20 15:19:43 +0000

    second push

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,6 +1,14 @@
-# WRITE YOUR FUNCTIONS HERE
+
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
 
-def get_total_cash(cash):
-    return cash["admin"]["total_cash"]
+def get_total_cash(tot_cash):
+    return tot_cash["admin"]["total_cash"]
+
+def add_or_remove_cash():
+
+def get_pets_sold(pets_gone):
+    return pets_gone["admin"]["pets_sold"]
+
+def get_stock_count(count):
+    
