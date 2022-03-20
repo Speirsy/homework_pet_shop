5f166e1b761e8e9cc3d6a1dde3561a4a453b1e20
Speirsy <jpspeirs@icloud.com>
2022-03-20 13:35:50 +0000

    first push

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+from src.pet_shop import *
+from tests.pet_shop_test import *
+
+if __name__ == '__main__':
+    unittest.main()
+
+def pet_shop_name():
+    return pet_shop_name
---
+++ src/pet_shop.py
@@ -0,0 +1,6 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(shop_name):
+    return shop_name["name"]
+
+def get_total_cash(cash):
+    return cash["admin"]["total_cash"]
